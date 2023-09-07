fa0fed8df9ae19ab65f0fb13a2265450f57c0530
Sachin Sagar <sachinssagar@gmail.com>
2023-09-07 17:02:12 +0530

    Somechange

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,19 +1,10 @@
-import { useCallback, useState, useEffect } from "react";
 import "./ProductList.css";
+import { useState } from "react";
+import { useFetch } from "../hooks/useFetch";
 
 export const ProductList = () => {
-  const [products, setProducts] = useState([]);
   const [url, setUrl] = useState("http://localhost:8000/products");
-
-  const fetchProducts = useCallback(async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    setProducts(data);
-  }, [url]);
-
-  useEffect(() => {
-    fetchProducts();
-  }, [fetchProducts]);
+  const { data: products } = useFetch(url);
 
   return (
     <section>
@@ -25,18 +16,19 @@ export const ProductList = () => {
       >
         In Stock
       </button>
-      {products.map((product) => (
-        <div className="product" key={product.id}>
-          <p>{product.id}</p>
-          <p>{product.name}</p>
-          <p>
-            <span>${product.price}</span>
-            <span className={product.in_stock ? "in-stock" : "out-of-stock"}>
-              {product.in_stock ? "In Stock" : "Out of Stock"}
-            </span>
-          </p>
-        </div>
-      ))}
+      {products &&
+        products.map((product) => (
+          <div className="product" key={product.id}>
+            <p>{product.id}</p>
+            <p>{product.name}</p>
+            <p>
+              <span>${product.price}</span>
+              <span className={product.in_stock ? "in-stock" : "out-of-stock"}>
+                {product.in_stock ? "In Stock" : "Out of Stock"}
+              </span>
+            </p>
+          </div>
+        ))}
     </section>
   );
 };
--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,1 +1,16 @@
 import { useState, useEffect } from "react";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const result = await response.json();
+      setData(result);
+    };
+    fetchData();
+  }, [url]);
+
+  return { data };
+};
